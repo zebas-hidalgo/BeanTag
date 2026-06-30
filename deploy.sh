fb568df6f4c19dc2b42f9cc00c2212ef2308a7ae
zebas <zebas@MacBook-Air-de-zebas.local>
2026-06-30 11:33:48 -0400

    fix: resolve expect quotes in deploy.sh and secure permissions

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set timeout 300
 
 # 1. Comprimir en local
-exec tar --exclude="node_modules" --exclude=".git" --exclude="frontend/node_modules" --exclude="backend/node_modules" -czf /tmp/beantag_update.tar.gz -C /Users/zebas/Desktop/Proyecto_cafe .
+exec tar --exclude=node_modules --exclude=.git --exclude=frontend/node_modules --exclude=backend/node_modules -czf /tmp/beantag_update.tar.gz -C /Users/zebas/Desktop/Proyecto_cafe .
 
 # 2. Subir archivo
 spawn scp -o StrictHostKeyChecking=no /tmp/beantag_update.tar.gz root@5.189.152.68:/root/beantag_update.tar.gz
